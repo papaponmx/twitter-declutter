f6296e21c00f0352ff165b109241da18f6767737
papaponmx <jaime.rios.dev@outlook.com>
2020-08-21 16:10:27 -0500

    Added selectors.

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,2 +1,6 @@
 // TODO: Within this script we can update the functionality for querying and updating the DOM.
 console.log('✅ Inside of content script');
+
+// Remove items from the DOM;
+document.querySelector('div[aria-label="Timeline: Trending now"]').style.display = "none";
+document.querySelector('aside[aria-label="Who to follow"]').style.display = "none";
